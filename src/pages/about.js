9a77edcbc2412a877bfad87a26fefe18be290ddd
asif-96 <shuyeb.dk@gmail.com>
2021-10-08 14:35:42 +0600

    add all files but still working

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet laudantium necessitatibus commodi quia fugiat modi impedit dolorem ipsa, ex sit!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet laudantium necessitatibus commodi quia fugiat modi impedit dolorem ipsa, ex sit!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet laudantium necessitatibus commodi quia fugiat modi impedit dolorem ipsa, ex sit!</p>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h2>DevPals</h2>
+      <h4>Software Solution</h4>
+      <p>Software Design and Development</p>
+    </div>
+  )
 }
